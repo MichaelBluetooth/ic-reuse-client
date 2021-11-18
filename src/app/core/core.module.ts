d34017632cb098bfc583b20a7d5852788a7a61fd
matthew bischoff <matthew.bischoff@3ds.com>
2021-11-18 13:56:53 -0500

    directive to detect when use it done typing

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,19 +6,22 @@ import { AlertComponent } from './components/alert/alert.component';
 import { LoginComponent } from './components/login/login.component';
 import { PricePipe } from './pipes/price/price.pipe';
 import { ToListingImagePipe } from './pipes/to-listing-image/to-listing-image.pipe';
+import { DoneTypingDirective } from './directives/done-typing/done-typing.directive';
 
 @NgModule({
   declarations: [
     AlertComponent,
     LoginComponent,
     PricePipe,
-    ToListingImagePipe
+    ToListingImagePipe,
+    DoneTypingDirective
   ],
   exports: [
     AlertComponent,
     LoginComponent,
     PricePipe,
-    ToListingImagePipe
+    ToListingImagePipe,
+    DoneTypingDirective
   ],
   imports: [
     CommonModule,
---
+++ src/app/core/directives/done-typing/done-typing.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, ElementRef, EventEmitter, Output } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+
+@Directive({
+  selector: '[appDoneTyping]',
+})
+export class DoneTypingDirective {
+  //This directive can output when the user is "done" typing
+  @Output() doneTyping = new EventEmitter();
+
+  //Inject the element reference that this directive was applied to
+  constructor(private eRef: ElementRef){}
+
+  ngOnInit(){
+    fromEvent(this.eRef.nativeElement, 'keyup')  //listen for the "keyup" events on the element this directive was applied to
+    .pipe(map((evt: any) => {
+      return evt.target.value //Specifically get the value so we know whether the user is modifying the value
+    }))
+    .pipe(debounceTime(3000)) //wait 3000ms between "keyup" events
+    .pipe(distinctUntilChanged()) //if the value changes, restart because the person is not done typing
+    .subscribe(() => {
+      this.doneTyping.next(); //they're done typing!
+    });
+  }
+}
